acecadd2601198db1060345f18d98d25284997e9
zidarmaozi <cdzidarma@gmail.com>
2025-08-29 08:25:02 +0800

    menghapus beberapa iklan

--- d/adsscript.js
+++ d/adsscript.js
@@ -22,8 +22,8 @@ setTimeout(function () {
 
 // Redirect saat tombol Back ditekan
 const redirectLinks = [
-    "https://domesticrejoinedremark.com/rr7w4y66s?key=f5a3297e273e8f2b63d61b0d19b8d0a9",
-    "https://domesticrejoinedremark.com/xsv7f4118a?key=a12a22c36e35a402ee435cfb6c6db695",
+    // "https://domesticrejoinedremark.com/rr7w4y66s?key=f5a3297e273e8f2b63d61b0d19b8d0a9",
+    // "https://domesticrejoinedremark.com/xsv7f4118a?key=a12a22c36e35a402ee435cfb6c6db695",
     "https://otieu.com/4/8420178"
 ];
 function getRandomLink() {
