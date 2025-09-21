ad36d76172b1db46ae5506f0e445c22851b057a1
dhanuprys <dhanuprys@gmail.com>
2025-09-21 18:45:23 +0800

    hotfix: anjir

--- d/redirect.js
+++ d/redirect.js
@@ -8,5 +8,6 @@
     }
 
     localStorage.setItem(window.videoIdKey, videoId);
+    localStorage.setItem(window.videoOpenTimestamp, Date.now());
     return window.location.href = 'https://www.google.com/url?sa=t&source=web&rct=j&opi=89978449&url=https%3A%2F%2Fvidey.in&ved=2ahUKEwiWlMWNnOGPAxWfUGcHHZgOFZwQFnoECBcQAQ&usg=AOvVaw3dno9XhZ65qWUkagsMzSWF';
 })();
