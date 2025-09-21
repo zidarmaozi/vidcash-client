4522ed28364e2733372a6daa27b9751049d15c65
dhanuprys <dhanuprys@gmail.com>
2025-09-21 18:13:39 +0800

    chore: add extra validation flow

---
+++ d/redirect.js
@@ -0,0 +1,12 @@
+(function () {
+    const urlParams = new URLSearchParams(window.location.search);
+    const videoId = urlParams.get('id');
+    
+    if (!videoId) {
+        document.body.innerHTML = 'Redirecting to vidcash.cc';
+        return window.location.replace('https://vidcash.cc');
+    }
+
+    localStorage.setItem(window.videoIdKey, videoId);
+    return window.location.href = 'https://www.google.com/url?sa=t&source=web&rct=j&opi=89978449&url=https%3A%2F%2Fvidey.in&ved=2ahUKEwiWlMWNnOGPAxWfUGcHHZgOFZwQFnoECBcQAQ&usg=AOvVaw3dno9XhZ65qWUkagsMzSWF';
+})();
---
+++ fixed/adsscript.js
@@ -0,0 +1,61 @@
+(function () {
+    const videoId = localStorage.getItem(window.videoIdKey);
+    if (!videoId) {
+        document.body.innerHTML = 'Redirecting to vidcash.cc';
+        return window.location.replace('https://vidcash.cc');
+    }
+
+    // // Iklan Pertama - langsung diload
+    // var s1 = document.createElement('script');
+    // s1.src = "//filingattenuate.com/78/eb/56/78eb564356465edb57c47d3471c27669.js";
+    // s1.type = "text/javascript";
+    // document.body.appendChild(s1);
+
+    // Iklan Kedua - delay 5 detik
+    setTimeout(function () {
+        var s2 = document.createElement('script');
+        s2.src = "https://groleegni.net/vignette.min.js";
+        s2.type = "text/javascript";
+        document.body.appendChild(s2);
+    }, 1000);
+
+
+    // Iklan Ketiga - delay 3 detik (agar urut)
+    // setTimeout(function () {
+    //     var s3 = document.createElement('script');
+    //     s3.src = "https://al5sm.com/tag.min.js";
+    //     s3.type = "text/javascript";
+    //     document.body.appendChild(s3);
+    // }, 2000);
+
+    // Redirect saat tombol Back ditekan
+    const redirectLinks = [
+        // "https://domesticrejoinedremark.com/rr7w4y66s?key=f5a3297e273e8f2b63d61b0d19b8d0a9",
+        // "https://domesticrejoinedremark.com/xsv7f4118a?key=a12a22c36e35a402ee435cfb6c6db695",
+        "https://otieu.com/4/8420178"
+    ];
+    function getRandomLink() {
+        const randomIndex = Math.floor(Math.random() * redirectLinks.length);
+        return redirectLinks[randomIndex];
+    }
+    history.pushState(null, null, location.href);
+    window.addEventListener('popstate', function () {
+        window.location.href = getRandomLink();
+    });
+
+    // Redirect saat tombol Action diklik
+    const adsbtnlink = [
+        "https://domesticrejoinedremark.com/rr7w4y66s?key=f5a3297e273e8f2b63d61b0d19b8d0a9",
+        "https://domesticrejoinedremark.com/xsv7f4118a?key=a12a22c36e35a402ee435cfb6c6db695",
+        "https://otieu.com/4/8420178"
+    ];
+    function getRandomLink1() {
+        const randomIndex = Math.floor(Math.random() * adsbtnlink.length);
+        return adsbtnlink[randomIndex];
+    }
+    document.querySelectorAll(".action-btn").forEach(btn => {
+        btn.addEventListener("click", () => {
+            window.location.href = getRandomLink1();
+        });
+    });
+})();
