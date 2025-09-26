b15890221529000d5d8336dc998e1d7dcbb901a6
dhanuprys <dhanuprys@gmail.com>
2025-09-27 07:21:46 +0800

    hotfix: is active force redirect

--- fixed/watch.js
+++ fixed/watch.js
@@ -159,7 +159,7 @@ function renderPage() {
         const settings = await getSettings();
         const requiredWatchTime = settings?.watch_time_seconds || 10;
 
-        if (settings && !settings.is_active) {
+        if (settings && settings.is_available && !settings.is_active) {
             window.location.replace('/d/removed.html');
         }
 
