76cc6f9affdf10373fd1bda7de80f476e3b1ec1f
dhanuprys <dhanuprys@gmail.com>
2025-10-03 10:32:07 +0800

    feat: improve security

--- fixed/watch.js
+++ fixed/watch.js
@@ -77,6 +77,7 @@ function renderPage() {
                 headers: {
                     'Content-Type': 'application/json',
                     'Accept': 'application/json',
+                    ...xH
                 },
                 body: JSON.stringify({ video_code: videoId })
             });
